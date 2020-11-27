65ed71e805897f1e8f48d3eac40dadc103c86878
LDxion2000 <leonkdixon@gmail.com>
2020-11-27 23:15:54 +0000

    login inputs added

---
+++ src/components/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="login">
+    <p>Hello</p>
+    <form action=""><div class="row">
+    <label for="username">Username</label>
+    <input id="username" type="text" v-model="username" />
+    </div>
+    
+    <div class="row">
+    <label for="Password">Password</label>
+    <input id="Password" type="text" v-model="password" />
+    </div></form>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+};
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import bootstrap from 'bootstrap'
+createApp(App)
+    .use(bootstrap)
+    .mount('#app')
